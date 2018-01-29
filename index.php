695a69114d970ff152bd40bc8fa358e7566e4059
Manpaul1 <01paulmanoni@gmail.com>
2018-01-29 15:00:45 +0300

    nimeanza kuandika index page

--- index.php
+++ index.php
@@ -1,1 +1,89 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+      <?php require_once 'templates/header.php';?>
+    </head>
+<body>
+    <!--Header-->
+  <?php require_once 'templates/heading.php';?>
+<!--here a registration form starts-->
+<div class="container">
+
+              
+                    </body>
+                    </html>
+
+                </div>
+
+        </div>
+    </div>
+</section>
+<section class="container-fluid">
+<hr class="featurette-divider">
+<div class="container-fluid" >
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="page-header">How to Apply for a loan.<br>
+             <span class="text-muted">It's Easy</span></h2>
+          <p class="lead">YEs! you just need a Membership account and then after being full Registred<br>You will fill up the loan application form and wait for approval
+        from our loan officers<br> and you would have your loan in no time</p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-fluid mx-auto" src="image/home.jpg" alt="Generic placeholder image">
+        </div>
+      </div>
+     </div>
+    </section>
+
+<!--Footer-->
+<footer id="footer">
+     <!--Container-->
+     <div class="continer-fluid" id="footer">
+
+                <!--row-fluids-->
+                <div class="container">
+
+                    <!--Contact Form-->
+                    <div class="span3" id="span3">
+                        <h4>ADDRESS</h4>
+                        <ul class="unstyled address">
+                                <li>
+                                        <i class="icon-home"></i></strong>Institute Of finance Management<br>
+                                    </li>
+                            <li>
+                                <i class="icon-home"></i></strong>Dar Es Salam<br>Tanzania
+                            </li>
+                            <li>
+                                <i class="icon-envelope"></i>
+                                <strong>Email: </strong> lvavalai@gmail.com
+                            </li>
+                            <li>
+                                <i class="icon-globe"></i>
+                                <strong>Website:</strong> www.ifmso.ac.tz
+                            </li>
+                            <li>
+                                <i class="icon-phone"></i>
+                                <strong>Tel:</strong> +255654028211</li>
+                                <li>
+                                <i class="copyright">&copy <strong>2017</strong> <br>Ifm Student Bcs Project </i>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+     </div>
+</footer>
+<!--/Footer-->
+
+</div>
+
+
+
+</body>
+
+<!-- this place under to make the web load faster -->
+    <script type="text/javscript" src="js/bootstrap.min.js"></script>
+    <script type="text/javscript" src="js/jquery.js"></script>
+    <script type="text/javscript" src="js/popper.js"></script>
+    <script type="text/javscript" src="js/vendor.js"></script>
+
+</html>
