e4ceea7f3a84ced7f4af670061b48a943cba0e8d
Manpaul1 <01paulmanoni@gmail.com>
2018-02-04 16:37:04 +0300

    adding Core folder

---
+++ Core/Config.php
@@ -0,0 +1,6 @@
+<?php
+
+!defined("db_server")?null : define("db_server","locahost");
+!defined("db_user")?null : define("db_user","root");
+!defined("db_pass")?null : define("db_pass","");
+!defined("db_name")?null : define("db_name","loan");
